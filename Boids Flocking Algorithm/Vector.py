2626e78615bdb9b69c7f94a6c637330c6166c819
Wilco Tromp <85456772+WilcoTromp@users.noreply.github.com>
2024-03-27 09:10:13 +0200

    Updated exe file

--- Boids Flocking Algorithm/Vector.py
+++ Boids Flocking Algorithm/Vector.py
@@ -31,16 +31,14 @@ class Vector:
 		self.x = self.x + a.x
 		self.y = self.y + a.y
 
-	def parseToInt(self):
-		return (int(self.x), int(self.y))
-
 	def magnitude(self):
 		return sqrt(self.x * self.x + self.y * self.y)
 
 	def normalize(self):
 		mag = self.magnitude()
-		if not (mag == 0 ):
+		if not (mag == 0):
 			self = self/mag
+
 	def Normalize(self):
 		mag = self.magnitude()
 		if mag != 0:
@@ -60,6 +58,7 @@ class Vector:
 			self.y = self.y/sqrt(squared_mag)
 			self.x = self.x * max_length
 			self.y = self.y * max_length
+
 	def reset(self, x=0, y=0):
 		self.x = x
 		self.y = y
@@ -67,19 +66,8 @@ class Vector:
 	def __repr__(self):
 		return f'vector-> x:{self.x}, y:{self.y}'
 
-
 def getDistance(v1, v2):
 	return sqrt((v2.x - v1.x)*(v2.x - v1.x) + (v2.y -v1.y)*(v2.y - v1.y))
-
-def AddVectors(v1, v2):
-	return Vector(v1.x + v2.x, v1.y + v2.y)
-
-def translate(value, min1, max1, min2, max2):
-    return min2 + (max2 - min2)* ((value-min1)/(max1-min1))
-
-def hsv_to_rgb(h, s, v):
-    return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h, s, v))
-
 
 def SubVectors(v1, v2):
 	return Vector(v1.x - v2.x, v1.y - v2.y)
